e998479bd28ba01200a423c6f63d2f77d879c6d3
dmitrijorlov78798@gmail.com <dmitrijorlov78798@gmail.com>
2024-05-03 05:04:41 +0300

    change2

--- Queke/System.cpp
+++ Queke/System.cpp
@@ -1,24 +1,60 @@
 #include "MainHeader.h"
 
-class systemAbs
-{
-public:
-	virtual void sleep(uint32_t ms) = 0;
-};
+static system* system::p_self = 0;
 
-class systemWin : public systemAbs
+system::system() : p_abs(0)
 {
-	void sleep(uint32_t ms) override;
-};
+#ifdef __WINDOWS__
+	p_abs = new systemWin;
+#endif // __WINDOWS__
+	if (p_abs == 0)
+		p_abs = new systemWin;
+}
 
-class system
+system* system::instance()
 {
-private:
-	systemAbs* p_abs;
-	static system* p_self;
-public:
-	static system* instance();
-	void sleep(uint32_t ms);
-protected:
-	system();
-};
+	if (p_self == 0)
+		p_self = new system;
+	return p_self;
+}
+
+void system::sleep(uint32_t ms)
+{
+	p_abs->sleep(ms);
+}
+
+
+//exch 
+void system::updateBitMap(bitMap& map)
+{
+	p_abs->updateBitMap(map);
+}
+//exch end
+
+
+//exch 
+systemWin::systemWin() : firstRun(true), sizeWindowX(0), sizeWindowY(0)
+{
+
+	hwnd = GetConsoleWindow();
+	hdc = GetDC(hwnd);
+}
+//exch end
+
+systemWin::sleep(uint32_t ms)
+{
+	Sleep(ms);
+}
+//exch 
+void systemWin::updateBitMap(bitMap& map) //добавить const iter
+{
+	if (!firstRun)
+	{
+		bitMap::Iterator iter = map.iterator.begin();
+		COLORREF color;
+
+		for (int y = 0; y < sizeWindowY && iter != map.iterator.end(); ++y, ++iter)// убрать iterator в map.iterator.end() 
+			for (int x = 0; x < sizeWindowX && iter != map.iterator.end(); ++x, ++iter)
+				SetPixel(hdc, x, y, RGB(iter->red(), iter->green(), iter->blue());
+	}
+}
--- Queke/System.h
+++ Queke/System.h
@@ -5,10 +5,105 @@
 
 #include "MainHeader.h"
 
+//exch
+class bitMapPixel
+{
+private:
+	uint8_t red, green, blue;
+public:
+	bitMapPixel() : red(0), green(0), blue(0)
+	{}
+	bitMapPixel(uint8_t red, uint8_t green, uint8_t blue) : red(red), green(green), blue(blue)
+	{}
+	uint8_t red()
+	{
+		return red;
+	}
+	uint8_t green()
+	{
+		return green;
+	}
+	uint8_t blue()
+	{
+		return blue;
+	}
+};
+
+
+class bitMap
+{
+private:
+	static const uint32_t SIZE_X = 600;
+	static const uint32_t SIZE_Y = 400;
+	bitMapPixel map[SIZE_Y][SIZE_X];
+public:
+	class Iterator
+	{
+	private:
+		uint32_t X, Y;
+		bitMap* p_map;
+	public:
+		Iterator() : X(0), Y(0), p_map(0)
+		{}
+		Iterator(uint32_t X, uint32_t Y, bitMap* p_map) : X(X), Y(Y), p_map(p_map)
+		{}
+		Iterator& operator++ ()
+		{
+			if (Y < 600)
+				if (++X == 600)
+				{
+					X = 0; Y++;
+				}
+			return *this;
+		}
+		Iterator begin()
+		{
+			return Iterator(0, 0, p_map);
+		}
+		Iterator end()
+		{
+			return Iterator(0, 600, p_map);
+		}
+		bool operator!= (Iterator& const rValue)
+		{
+			return Y != rValue.Y || X != rValue.X;
+		}
+		bitMapPixel& operator * ()
+		{
+			X = X > SIZE_X || X < 0 ? 0 : X;
+			Y = Y > SIZE_Y || Y < 0 ? 0 : Y;
+			return p_map->map[Y][X];
+		}
+		bitMapPixel* operator -> ()
+		{
+			X = X > SIZE_X || X < 0 ? 0 : X;
+			Y = Y > SIZE_Y || Y < 0 ? 0 : Y;
+			return &p_map->map[Y][X];
+		}
+		/*bool operator < (iterator& const rValue)
+		{
+			bool result = false;
+
+			if (Y == rValue.Y)
+				result = X < rValue.X;
+			else
+				result = Y < rValue.Y;
+
+			return result;
+		}*/
+	};
+	Iterator iterator;
+	bitMap() : iterator(0, 0, this)
+	{}
+};
+//end
+
+
 class cube
 {
 	uint8_t alfa, red, green, blue;
 };
+
 
 class actor;
 
@@ -31,6 +126,41 @@ public:
 	};
 };
 
+
+class systemAbs
+{
+public:
+	virtual void sleep(uint32_t ms) = 0;
+	virtual void updateBitMap(bitMap& map) = 0;//exch
+};
+
+class systemWin : public systemAbs
+{
+private://exch 
+	HWND hwnd;//exch 
+	HDC hdc;//exch 
+	bool firstRun;//exch 
+	int sizeWindowX;//exch 
+	int sizeWindowY;//exch 
+public://exch 
+	systemWin();//exch
+	void sleep(uint32_t ms) override;
+	void updateBitMap(bitMap& map) override;//exch
+};
+
+class system
+{
+private:
+	systemAbs* p_abs;
+	static system* p_self;
+public:
+	static system* instance();
+	void sleep(uint32_t ms);
+	void updateBitMap(bitMap& map);//exch
+protected:
+	system();
+	
+};
 
 #endif // !SYSTEM
 
